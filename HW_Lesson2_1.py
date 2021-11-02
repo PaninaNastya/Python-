acef8de485592ff2e6dcd236239ec793dfbe23ad
PaninaNastya <86881642+PaninaNastya@users.noreply.github.com>
2021-11-02 10:47:44 +0300

    Add files via upload

---
+++ HW_Lesson2_1.py
@@ -0,0 +1,16 @@
+print(isinstance((15 * 3), int))
+print(type(15 * 3))
+print()
+
+print(isinstance((15 / 3), int))
+print(type(15 / 3))
+"Результат деления в Python 3 и выше всегда приводится к типу float, который требует большее количество памяти"
+print()
+
+print(isinstance((15 // 3), int))
+print(type(15 // 3))
+print()
+
+print(isinstance((15 ** 3), int))
+print(type(15 ** 3))
+
