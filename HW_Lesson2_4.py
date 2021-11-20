104ac6717b23d7dde29e4d7fc873a2b90e5a184f
PaninaNastya <86881642+PaninaNastya@users.noreply.github.com>
2021-11-20 18:49:55 +0300

    Update HW_Lesson2_4.py

--- HW_Lesson2_4.py
+++ HW_Lesson2_4.py
@@ -1,6 +1,6 @@
 profession = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
 
 for i in range(len(profession)):
-    temporary_list = profession[i].split(" ")
+    temporary_list = profession[i].split()
     name = temporary_list[-1].lower().title()
     print(f"Привет, {name}!")
